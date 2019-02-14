0b9860cc453ad88c192a3cf3f5d2d48311340365
Kristina U. Kosina <vagrant@vagrant.vm>
2019-02-14 20:31:51 +0000

    fixed secret.sh in helper.py

--- helper.py
+++ helper.py
@@ -1,11 +1,22 @@
+import keepaAPI
+import os
+
+#os.system("source secrets.sh")
+
+#get Keepa API acessskey from secrets.sh
+accesskey = os.environ["API_KEY"]
 
 
 def get_product_id(amazon_url):
-    """ When Amazon url string is presented in the search field, return product id"""
+    """ When Amazon url string is presented in the search field, return 
+    product id
+    """
+
     amazon_url.replace("?", "/")
     split_url_list = amazon_url.split("/")
 
     for i in range(0, len(split_url_list) - 1):
+
         if split_url_list[i] == "dp":
             product_id = split_url_list[i+1]
             return product_id
@@ -15,7 +26,6 @@ def get_product_data(product_id):
     """ Send request to get all product data"""
     
     api = keepaAPI.API(accesskey)
-
     products = api.ProductQuery(product_id)
 
     return products[0]
--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 from flask import Flask, redirect, request, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
-import keepaAPI
-import os
 from helper import get_product_id, get_product_data
+# from seed import load
 
 #os.system("source secrets.sh")
 
@@ -13,7 +12,7 @@ app.jinja_env.auto_reload = True
 
 app.secret_key = "ABC"
 
-accesskey = os.environ["API_KEY"]
+
 
 
 @app.route("/")
@@ -29,13 +28,14 @@ def get_prices():
     product_id = get_product_id(amazon_url)
     product = get_product_data(product_id)
 
+
     return render_template("get-prices.html", product=product)
 
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = False
+    app.debug = True 
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
