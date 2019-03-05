04e77234ccf1fef9388a1a9ea3397c0ee555b6cd
Kristina U. Kosina <vagrant@vagrant.vm>
2019-03-05 00:50:46 +0000

    more specific name for helper.py - query_helper.py

---
+++ query_helper.py
@@ -0,0 +1,40 @@
+import keepaAPI
+import os
+
+#os.system("source secrets.sh")
+
+#get Keepa API acessskey from secrets.sh
+accesskey = os.environ["API_KEY"]
+
+
+def get_amazon_id(amazon_url):
+    """ When Amazon url string is presented in the search field, return 
+    product id
+    """
+
+    modified_amazon_url = amazon_url.replace("?", "/")
+    split_url_list = modified_amazon_url.split("/")
+
+    for i in range(0, len(split_url_list) - 1):
+
+        if split_url_list[i] == "dp":
+            amazon_id = split_url_list[i+1]
+            print(amazon_id)
+            return amazon_id
+        elif split_url_list[i] == "product":
+            amazon_id = split_url_list[i+1]
+            print(amazon_id)
+            return amazon_id
+
+
+def get_product_data(amazon_id):
+    """ Send request to get all product data"""
+    
+    api = keepaAPI.API(accesskey)
+    products = api.ProductQuery(amazon_id)
+
+    return products
+
+
+
+
